8b2ea1728d19b6c1e6669bbbfe49369a25cd9239
Javier <javiervidal.1991@gmail.com>
2023-07-10 13:48:16 +0200

    Create a custom hook to fetch the games

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import apiClient from "../services/api-client";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState<string>("");
+
+  useEffect(() => {
+    apiClient
+      .get<FetchGamesResponse>("/games")
+      .then((res) => setGames(res.data.results))
+      .catch((err) => setError(err.message));
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
+
---
+++ src/services/vite.config.js
@@ -0,0 +1,7 @@
+import reactRefresh from '@vitejs/plugin-react-refresh';
+
+export default {
+    plugins: [reactRefresh()],
+    // Configura otras opciones de Vite según tus necesidades
+  };
+  
