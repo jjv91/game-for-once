4a345dfbd0da119fcde3fc580f3ab43ca71b2abd
Javier <javiervidal.1991@gmail.com>
2023-07-12 13:54:48 +0200

    Displaying credit score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Game } from "../hooks/useGames";
-import { Box, Image, Heading, Text } from "@chakra-ui/react";
+import { Box, Image, Heading, Text, HStack } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList"; // assuming the file is in the same directory
+import CriticScore from "./CriticScore";
 
 interface Props {
   game: Game;
@@ -13,9 +14,12 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} alt={game.name} />
       <Box p="6">
         <Heading fontSize="2xl">{game.name}</Heading>
-        <PlatformIconList
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
+        <HStack justifyContent={"space-between"}>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
       </Box>
     </Box>
   );
