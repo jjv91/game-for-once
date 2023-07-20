20100fc1d46b030a5cc389eb37a4099bccaef540
Javier <javiervidal.1991@gmail.com>
2023-07-20 18:44:50 +0200

    Fix th issue chakra menus

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, HStack, Box } from "@chakra-ui/react";
+import { Grid, GridItem, HStack, Box, Flex } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenereList";
@@ -44,20 +44,21 @@ function App() {
         </GridItem>
       </Box>
       <GridItem area="main">
-        <HStack spacing={5} paddingLeft={2} marginBottom={5}>
+        <Flex gap={5} paddingLeft={2} marginBottom={5}>
           <PlatformSelector
             selectedPlatform={gameQuery.platform}
             onSelectPlatform={(platform) =>
               setGameQuery({ ...gameQuery, platform })
             }
           />
+
           <SortSelector
             sortOrder={gameQuery.sortOrder}
             onSelectSortOrder={(sortOrder) =>
               setGameQuery({ ...gameQuery, sortOrder })
             }
           />
-        </HStack>
+        </Flex>
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
     </Grid>
--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -10,7 +10,7 @@ const SortSelector = ({ onSelectSortOrder, sortOrder }: Props) => {
   const sortOrders = [
     { value: "", label: "Relevance" },
     { value: "-added", label: "Date added" },
-    { value: "name", label: "Release date" },
+    { value: "name", label: "Name" },
     { value: "-released", label: "Realase date" },
     { value: "-metacritic", label: "Popularity" },
     { value: "-rating", label: "Average rating" },
